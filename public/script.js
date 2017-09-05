c6dfc91c387c58baed9c73a5855703bdf8f7ba11
Breyana Scales <breyanaj@gmail.com>
2017-09-05 15:58:30 -0700

    Add tabbing to fetch call and refactor jQuery li

--- public/script.js
+++ public/script.js
@@ -2,12 +2,12 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
   fetch('http://mutably.herokuapp.com/books', { method: 'GET' })
-  .then(response => response.json())
-  .then(json => {
-    const books = json.books
-    books.forEach(book => {
-      const li = $(`<li></li>`).text(book.title)
-      li.appendTo('.list-group')
+    .then(response => response.json())
+    .then(json => {
+      const books = json.books
+      books.forEach(book => {
+        const li = $(`<li>`).text(book.title)
+        li.appendTo('.list-group')
+      })
     })
-  })
 });
