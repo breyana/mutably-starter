2bc08407fbd32c2dba010a2e13261d0420e1ebfe
Breyana Scales <breyanaj@gmail.com>
2017-09-05 15:07:34 -0700

    Fetch books, surface titles to page

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
-  // code in here
-
+  fetch('http://mutably.herokuapp.com/books', { method: 'GET' })
+  .then(response => response.json())
+  .then(json => {
+    const books = json.books
+    console.log(books)
+    books.forEach(book => {
+      const li = $(`<li></li>`).text(book.title)
+      console.log(li)
+      li.appendTo('.list-group')
+    })
+  })
 });
