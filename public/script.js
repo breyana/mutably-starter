f09ea5bc260ec0e3e61c6807f8c48f167660ec86
Breyana Scales <breyanaj@gmail.com>
2017-09-05 15:08:51 -0700

    Remove console logging in fetch

--- public/script.js
+++ public/script.js
@@ -5,10 +5,8 @@ $(document).ready(function(){
   .then(response => response.json())
   .then(json => {
     const books = json.books
-    console.log(books)
     books.forEach(book => {
       const li = $(`<li></li>`).text(book.title)
-      console.log(li)
       li.appendTo('.list-group')
     })
   })
