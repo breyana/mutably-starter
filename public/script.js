37c1e8368ae6a2561ba6c375bf312a7a9e7f5099
Breyana Scales <breyanaj@gmail.com>
2017-09-07 13:20:29 -0700

    Add book ID to the li ID

--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
-console.log("Sanity Check: JS is working!");
-
 $(document).ready(function(){
   fetch('http://mutably.herokuapp.com/books', { method: 'GET' })
     .then(response => response.json())
     .then(json => {
+      console.log(json)
       const books = json.books
       books.forEach(book => {
         const button = $('<button>').text('Edit').attr('class', 'edit')
         const title = $('<span>').text(book.title).attr('class', 'title')
-        const bookLi = $('<li>').append(button).append(title).attr('class', 'book')
+        const bookLi = $('<li>').append(button).append(title).attr({
+          class: 'book',
+          id: book._id
+      })
         bookLi.appendTo('.list-group')
       })
     })
