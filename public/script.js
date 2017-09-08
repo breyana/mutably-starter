48ad9e5bd7f297e02c6c175f6ae6c880af5658e2
Breyana Scales <breyanaj@gmail.com>
2017-09-08 09:20:07 -0700

    Seperate functions into MVC groups, append input fields to each book

--- public/script.js
+++ public/script.js
@@ -1,17 +1,67 @@
 $(document).ready(function(){
-  fetch('http://mutably.herokuapp.com/books', { method: 'GET' })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json)
-      const books = json.books
-      books.forEach(book => {
-        const button = $('<button>').text('Edit').attr('class', 'edit')
-        const title = $('<span>').text(book.title).attr('class', 'title')
-        const bookLi = $('<li>').append(button).append(title).attr({
-          class: 'book',
-          id: book._id
-      })
-        bookLi.appendTo('.list-group')
-      })
-    })
+  CONTROLLER.showBooks()
 });
+
+var MODEL_BOOKS = {
+  findAll: function() {
+    return fetch('http://mutably.herokuapp.com/books', { method: 'GET' })
+      .then(response => response.json())
+      .then(json => json.books)
+  },
+  create: function(book) {
+    return fetch('mutably.herokuapp.com/books', {method: 'POST', body: book})
+  },
+  update: function(bookId) {
+  }
+}
+
+var UI = {
+  showBooks: function(books) {
+    books.forEach(book => {
+      const button = $('<button>').text('Edit').attr('class', 'edit')
+      const title = $('<span>').text(book.title).attr('class', 'title')
+      const editField = $('<input>').attr('value', book.title)
+      const bookLi = $('<li>').append(button).append(title).append(editField).attr({
+        class: 'book',
+        id: book._id
+      })
+      bookLi.appendTo('.list-group')
+    })
+  },
+  addBook: function(book) {
+
+  },
+  updateBook: function(book) {
+
+  },
+  addErrorMessage: function(error) {
+
+  },
+  editBook: function(event) {
+
+  },
+  addEventListeners: function() {
+    $('.edit').on('click', UI.editBook)
+  }
+}
+
+var CONTROLLER = {
+  addBook: function() {
+    var book = {} // use document.querySelector to get the book from the form
+    MODEL_BOOKS.create(book)
+    .then( createdBook => {
+      UI.addBook(createdBook)
+    })
+    .catch( error => {
+      // update the DOM s
+      UI.addErrorMessage(error)
+    })
+  },
+  showBooks: function() {
+    MODEL_BOOKS.findAll()
+      .then(books => {
+        UI.showBooks(books)
+        UI.addEventListeners()
+      })
+  }
+}
